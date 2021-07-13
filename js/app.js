f05293e28085db31b11f9da471bea5611385e3a2
lgLuis <luis_unlz@yahoo.com.ar>
2021-07-13 18:09:29 -0300

    oki doki

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 // BOTON FLOTANTE EN EL FOOTER
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    //      HEADER TRANSICION IMAGEN
+    const imagenHero=document.querySelector(".hero");
+    //necesito 2 variables
+    let i =0;
+    let t =0;
+
+    //Imagenes de fondo
+    const imagenes=["arriba2.jpg", "arriba.jpg"]
+
+    setInterval(()=>{
+        imagenHero.style.backgroundPositionX="-"+t+"px";
+        if(t>20){
+            t=0;
+            imagenHero.style.backgroundImage="url(../img/"+imagenes[i]+")";
+            if(i===imagenes.length -1) {
+                i=0;
+            }else{
+                i++;
+            }
+        // }else{
+        //     t++;
+         }
+
+        // t+=1;
+        // console.log(t);
+        t++;
+    },250);
+
+
+
+    //          FOOTER BOTON
     const btnFlotante = document.querySelector(".btn-flotante");  // . es una clase 
     // console.log(btnFlotante);
     btnFlotante.addEventListener("click", (e)=>{
